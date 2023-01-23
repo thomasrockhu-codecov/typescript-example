696897595f5d3727b4a8d86c486077db10778c39
Tom Hu <tomhu1096+gpg@gmail.com>
2023-01-22 22:20:06 -0500

    fix: add tests for subtract fn

--- app/calculator.spec.ts
+++ app/calculator.spec.ts
@@ -1,5 +1,9 @@
-import {  add } from './calculator'
+import { add, subtract } from './calculator'
 
 test('add function', () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test('subtract function', () => {
+  expect(subtract(1, 2)).toBe(-1);
 });
